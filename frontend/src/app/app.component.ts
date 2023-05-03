901a8d1b9b0fa73690c540472be68a23f1dc72f9
GuilhermeLLau <guilherme.laudecarvalho@gmail.com>
2023-05-02 21:00:53 -0300

    02 05 2023

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Tarefa } from "./tarefa";
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
 @Component({
  selector: 'app-root',
  templateUrl: './app.component.html',
@@ -10,6 +12,8 @@ export class AppComponent {
  title = 'TODOapp';
  arrayDeTarefas: Tarefa[] = [];
  apiURL : string;
+ tokenJWT = '{ "token":""}';
+ usuarioLogado = false;
  constructor(private http: HttpClient) {
  this.apiURL = 'http://localhost:3000';
  this.READ_tarefas();
@@ -19,10 +23,13 @@ export class AppComponent {
     this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
     resultado => { console.log(resultado); this.READ_tarefas(); });
  }
- READ_tarefas() {
-    this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
-    resultado => this.arrayDeTarefas=resultado);
- }   
+READ_tarefas() {
+ const idToken = new HttpHeaders().set("id-token", JSON.parse(this.tokenJWT).token);
+ this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`, { 'headers': idToken }).subscribe(
+ (resultado) => { this.arrayDeTarefas = resultado; this.usuarioLogado = true },
+ (error) => { this.usuarioLogado = false }
+ )
+}  
  DELETE_tarefa(tarefaAserRemovida: Tarefa) {
     var indice = this.arrayDeTarefas.indexOf(tarefaAserRemovida);
     var id = this.arrayDeTarefas[indice]._id;
@@ -35,5 +42,17 @@ export class AppComponent {
     this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`,
     tarefaAserModificada).subscribe(
     resultado => { console.log(resultado); this.READ_tarefas(); });
- }     
+ }
+ 
+ login(username: string, password: string) {
+   var credenciais = { "nome": username, "senha": password }
+   this.http.post(`${this.apiURL}/api/login`, credenciais).subscribe(resultado => {
+   this.tokenJWT = JSON.stringify(resultado);
+   this.READ_tarefas();;
+   });
+  }
+  
+
 }
+
+
